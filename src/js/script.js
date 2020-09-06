c581357ecbada8a1d05d16a0bd4e8df8df28246a
GentlemanMenace <kcrundquist@gmail.com>
2020-09-06 16:16:00 -0500

    Total now working.  Also added conditional statement to show or hide payment sections

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,11 @@ const colorDiv = document.getElementById('shirt-colors');
 const colorSelectorOptions = document.getElementById('color').children;
 const activities = document.querySelector('.activities');
 const activityCheckboxes = document.querySelectorAll('.activities input');
+const total = document.querySelector('[data-total]');
+const paymentSelector = document.getElementById('payment');
+const creditCardDiv = document.getElementById('credit-card');
+const paypalDiv = document.getElementById('paypal');
+const bitcoinDiv = document.getElementById('bitcoin');
 
 //This event listener shows or hides the other title textbox if 'other' is selected in the dropdown
 titleSelector.addEventListener('change', (e) => {
@@ -48,9 +53,18 @@ designSelector.addEventListener('change', (e) => {
 
 
 //This event listener checks if the clicked checkbox has any matching day and time checkboxes in the same fieldset.  This was learned through the FSJS Checkboxes Warm Up
-activities.addEventListener('click', (e) => {    
+activities.addEventListener('change', (e) => {    
     const clicked = e.target;
     const clickedDayAndTime = clicked.getAttribute('data-day-and-time');
+    let currentTotal = parseInt(total.textContent);
+    const clickedCost = parseInt(clicked.getAttribute('data-cost'));
+
+    if(clicked.checked) {
+        currentTotal += clickedCost;
+    } else {
+        currentTotal -= clickedCost;
+    }
+    total.innerHTML = currentTotal.toString();
 
     for(let i = 0; i < activityCheckboxes.length; i++) {
         const checkboxDayAndTime = activityCheckboxes[i].getAttribute('data-day-and-time');
@@ -68,10 +82,31 @@ activities.addEventListener('click', (e) => {
     }
 });
 
+paymentSelector.addEventListener('change', (e) => {
+    const paymentType = e.target.value;
+
+    if (paymentType === 'credit card') {
+        creditCardDiv.style.display = '';
+        paypalDiv.style.display = 'none';
+        bitcoinDiv.style.display = 'none';
+    } else if (paymentType === 'paypal') {
+        creditCardDiv.style.display = 'none';
+        paypalDiv.style.display = '';
+        bitcoinDiv.style.display = 'none';
+    }else if (paymentType === 'bitcoin') {
+        creditCardDiv.style.display = 'none';
+        paypalDiv.style.display = 'none';
+        bitcoinDiv.style.display = '';
+    }
+});
+
 //Initial Page Load set up below
 function init() {
     colorDiv.style.display = 'none';
     designSelector.firstElementChild.hidden = true;
+    paymentSelector.firstElementChild.hidden = true;
+    paypalDiv.style.display = 'none';
+    bitcoinDiv.style.display = 'none';
     nameTextBox.focus();
     otherTitleTextBox.style.display = 'none';
 }
